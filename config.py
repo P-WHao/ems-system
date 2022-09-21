99ded7aa3724004c77afc82ba065039a8acba793
P-WHao <haowenphoon@gmail.com>
2022-09-21 20:21:00 +0800

    update

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cxwfusms7e6g.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "phoonwenhao-employee"
+customregion = "us-east-1"
