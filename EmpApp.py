0bd0edff9e3fed4d8c4d646af28462dbc68f40cd
P-WHao <haowenphoon@gmail.com>
2022-09-22 18:57:52 +0800

    update

--- EmpApp.py
+++ EmpApp.py
@@ -75,8 +75,8 @@ def Emp():
             if returnQuery == 1:
                 print("Data inserted in MySQL RDS... uploading image to S3...")
                 s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-                bucket_location = boto3.client('s3').get_bucket_location(Bucket=customfolder)
-                s3_location = (bucket_location['LocationConstraint'])
+                bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+                s3_location = (bucket_location + "/" + customfolder)
                 # s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
                 # bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
                 # s3_location = (bucket_location['LocationConstraint'])
